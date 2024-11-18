d599a6394b9f1620246312dd25b9f63cbfefcec7
MartinsOyelade200727 <martins.oyelade@binghamuni.edu.ng>
2024-11-19 00:29:43 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+def calculate_impulse():
+    force=float(input('force'))
+    time=float(input('time'))
+    impulse=force*time
+
+    print('impulse is',impulse)
+calculate_impulse()
+
+
+def calculate_force():
+    mass=float(input('mass'))
+    acceleration=float(input('acceleration'))
+    force=mass*acceleration
+
+    print('force is',force)
+calculate_force()
+
+
+def calculate_velocity():
+    displacement=float(input('displacement'))
+    time=float(input('time'))
+    velocity=float(input('velocity'))
+
+
+    print('velocity is',velocity)
+calculate_velocity()
+
+
+def calculate_density():
+    mass=float(input('mass'))
+    volume=float(input('volume'))
+    density=float(input('density'))
+
+
+    print('density is',density)
+calculate_density()
+
+
+def calculate_electrical_charge():
+    current=float(input('current'))
+    time=float(input('time'))
+    electrical_charge=current*time
+
+
+    print('electrical_charge is',electrical_charge)
+calculate_electrical_charge()
+
+
